674dfaab9ff24cd78d74dea273ead235bf204c5f
Rukeshkannan01 <101107229+Rukeshkannan01@users.noreply.github.com>
2022-03-21 17:12:36 +0530

    Add files via upload

---
+++ 21-03-2022_DessertsCollections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_03_2022_LearningProcess
+{
+    class _21_03_2022_DessertsCollections
+    {
+        public static void Main()
+        {
+            Queue Desserts = new Queue();
+            Desserts.Enqueue("Gulab Jamuns");
+            Desserts.Enqueue("laddu");
+            Desserts.Enqueue("Payasam");
+            Desserts.Enqueue("Ice Cream");
+            /*foreach (string i in Desserts)
+                Console.WriteLine(i);
+            */
+            FileStream df = new FileStream("D:\\Softura training\\10_Day\\21-03-2022_DessertsCollections.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(df);
+            bw.Write("Desserts lists : \n");
+            foreach (string coll in Desserts)
+            {
+                bw.Write("\n"+coll);
+            }
+            //bw.BaseStream.Seek(0,SeekOrigin.End);
+            //bw.Flush();
+            bw.Close();
+            df.Close();
+
+            FileStream fs1 = new FileStream("D:\\Softura training\\10_Day\\21-03-2022_DessertsCollections.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs1);
+            string str = br.ReadString();
+            fs1.Close();
+
+            FileInfo fi = new FileInfo("D:\\Softura training\\10_Day\\21-03-2022_DessertsCollections.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+
+            Console.Read();
+           
+
+        }
+    }
+}
---
+++ 21-03-2022_EmployeeClass .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace _21_03_2022_LearningProcess
+{
+    class _21_03_2022_EmployeeClass
+    {
+        public static void Main()
+        {
+            Dictionary<int, string> dClass = new Dictionary<int, string>();
+            dClass.Add(1, "Name: Nithin, Gender: Male, Age: 21");
+            dClass.Add(2, "Name: Kavi, Gender: Male, Age: 21");
+            dClass.Add(3, "Name: Rukeshkumar, Gender: Male, Age: 21");
+            FileStream df = new FileStream("D:\\Softura training\\10_Day\\21-03-2022_EmployeeClass.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(df);
+            foreach (var v in dClass)
+                Console.WriteLine("Employee Id: {0}  {1}", v.Key, v.Value);
+            foreach (var v in dClass)
+            {
+                bw.Write(v.Key + v.Value);
+                bw.Write("\n");
+            }
+
+            bw.Flush();
+            bw.Close();
+            df.Close();
+
+            Console.Read();
+        }
+    }
+}
