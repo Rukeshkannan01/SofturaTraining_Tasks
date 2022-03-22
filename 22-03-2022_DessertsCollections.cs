c4bac8f6529af12d1d6d21db47eb94d866bd1e85
Rukeshkannan01 <101107229+Rukeshkannan01@users.noreply.github.com>
2022-03-22 16:23:31 +0530

    Add files via upload

---
+++ 22-03-2022_DessertsCollections.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_03_2022_LearningProcess
+{
+    class _21_03_2022_DessertsCollections
+    {
+        public static void Main()
+        {
+            Queue Desserts = new Queue();
+            Desserts.Enqueue("Gulab Jamuns");
+            Desserts.Enqueue("laddu");
+            Desserts.Enqueue("Payasam");
+            Desserts.Enqueue("Ice Cream");            
+            FileStream df = new FileStream("D:\\Softura training\\11_Day\\22-03-2022_DessertsCollections.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(df);
+            bw.Write("Desserts lists : \n");
+            foreach (string coll in Desserts)
+            {
+                bw.Write("\n"+coll);
+            }           
+            bw.Close();
+            df.Close();
+
+            FileStream fs1 = new FileStream("D:\\Softura training\\11_Day\\22-03-2022_DessertsCollections.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs1);
+            string str = br.ReadString();
+            fs1.Close();
+
+            FileInfo fi = new FileInfo("D:\\Softura training\\11_Day\\21-03-2022_DessertsCollections.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+
+            Console.Read();
+           
+
+        }
+    }
+}
---
+++ 22-03-2022_SerialEmployClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace _22_03_2022_LearningProcess
+{
+    [Serializable]
+    class ClassEmployee
+    {
+        public int Id = 1;
+        public string Name = "EmployeeName01";
+        public string Gender = "Male";
+        public int Age = 21;        
+
+    }
+    class _22_03_2022_SerialEmployClass
+    {
+        public void SerialEmployee()
+        {
+            ClassEmployee Emply = new ClassEmployee();
+            FileStream Efile = new FileStream(@"D:\Softura training\11_Day\SerialEmployee.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            SoapFormatter EfileSf = new SoapFormatter();
+            EfileSf.Serialize(Efile, Emply);
+            Efile.Close();
+
+        }
+        public void DeserialEmployee()
+        {
+            FileStream Efile = new FileStream(@"D:\Softura training\11_Day\SerialEmployee.txt", FileMode.Open, FileAccess.Read);
+            SoapFormatter EfileSf = new SoapFormatter();
+            ClassEmployee Emply01 = (ClassEmployee)EfileSf.Deserialize(Efile);
+            Console.WriteLine(Emply01.Id);
+            Console.WriteLine(Emply01.Name);
+            Console.WriteLine(Emply01.Gender);
+            Console.WriteLine(Emply01.Age);
+
+
+        }
+        public static void Main()
+        {
+            _22_03_2022_SerialEmployClass Eobj = new _22_03_2022_SerialEmployClass();
+            Eobj.SerialEmployee();
+            Eobj.DeserialEmployee();
+            Console.Read();
+
+
+        }
+    }
+}
